1989a284cbda23973e54ca1a93b7a78505879373
maskname <sunandrainy@sina.com>
2022-09-15 23:19:20 +0800

    提交MASTER

---
+++ src/test/java/test2/Test2BranchTest.java
@@ -0,0 +1,11 @@
+package test2;
+
+/**
+ * 2022/9/15
+ * author:ljh
+ */
+public class Test2BranchTest {
+    public static void main(String[] args) {
+        System.out.println("Test2BranchTest");
+    }
+}
