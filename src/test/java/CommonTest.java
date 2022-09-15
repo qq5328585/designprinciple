368166830ac450047c0190afc29fbaab9c72c67d
maskname <sunandrainy@sina.com>
2022-09-15 19:07:05 +0800

    主分支提交主分支代码、公共代码

---
+++ src/test/java/CommonTest.java
@@ -0,0 +1,9 @@
+/**
+ * 2022/9/15
+ * author:ljh
+ */
+public class CommonTest {
+    public static void main(String[] args) {
+        System.out.println("CommonTest");
+    }
+}
---
+++ src/test/java/master/MasterBranchTest.java
@@ -0,0 +1,11 @@
+package master;
+
+/**
+ * 2022/9/15
+ * author:ljh
+ */
+public class MasterBranchTest {
+    public static void main(String[] args) {
+        System.out.println("MasterBranchTest");
+    }
+}
