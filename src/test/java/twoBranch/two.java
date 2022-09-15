11a7d275fa9ca9a06e40c3185354d70d4e352a2f
maskname <sunandrainy@sina.com>
2022-09-15 16:52:05 +0800

    two branch two

---
+++ src/test/java/twoBranch/two.java
@@ -0,0 +1,11 @@
+package twoBranch;
+
+/**
+ * 2022/9/15
+ * author:ljh
+ */
+public class two {
+    public static void main(String[] args) {
+        System.out.println("This is two branch two");
+    }
+}
