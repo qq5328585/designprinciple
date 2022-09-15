fa0b1289c80b43e37e9d9a3c23f9b71a42a8928b
maskname <sunandrainy@sina.com>
2022-09-15 18:27:44 +0800

    first branch four

---
+++ src/test/java/firastBranch/four.java
@@ -0,0 +1,11 @@
+package firastBranch;
+
+/**
+ * 2022/9/15
+ * author:ljh
+ */
+public class four {
+    public static void main(String[] args) {
+        System.out.println("This is first batch four");
+    }
+}
