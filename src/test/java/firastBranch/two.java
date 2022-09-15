4986b3e0c1bc0977224aa8823f49459127c3df40
maskname <sunandrainy@sina.com>
2022-09-15 16:53:46 +0800

    first branch two and three

---
+++ src/test/java/firastBranch/two.java
@@ -0,0 +1,11 @@
+package firastBranch;
+
+/**
+ * 2022/9/15
+ * author:ljh
+ */
+public class two {
+    public static void main(String[] args) {
+        System.out.println("This is first branch two");
+    }
+}
