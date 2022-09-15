e1e108702aac58b25eb38663f5df88bebe079b6d
maskname <sunandrainy@sina.com>
2022-09-15 23:53:02 +0800

    主分支添加***

--- src/test/java/CommonTest.java
+++ src/test/java/CommonTest.java
@@ -4,6 +4,6 @@
  */
 public class CommonTest {
     public static void main(String[] args) {
-        System.out.println("===master===");
+        System.out.println("***===master===***");
     }
 }
